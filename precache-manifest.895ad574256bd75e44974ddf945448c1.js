108d0bb36083b2331564bc224ceb47cb9ce8a2cc
Juandozco <juandozco@hotmail.com>
2020-04-06 20:09:49 +0900

    Updates

---
+++ precache-manifest.895ad574256bd75e44974ddf945448c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1804b685de74c4b9eff661ba8346188e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca24748bb4c2748041cf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99b6543b9afd291cbdd3",
+    "url": "/static/js/2.4c329a4d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4c329a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca24748bb4c2748041cf",
+    "url": "/static/js/main.3506794b.chunk.js"
+  },
+  {
+    "revision": "36d47255b26284064c8a",
+    "url": "/static/js/runtime-main.4f99639b.js"
+  }
+]);
